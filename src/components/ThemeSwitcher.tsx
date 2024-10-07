d8d21522cdd7655231092dc3a21b866fbd3e3ba2
PonomarenkoA <aponomarenko@ukr.net>
2024-10-07 18:33:32 +0300

    fixed width

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -20,10 +20,10 @@ export const ThemeSwitcher = () => {
 				<li>
 					<button
 						onClick={() => setTheme('theme1')}
-						className='relative flex h-[44px] w-[44px] flex-wrap rounded-[5px] border-[2px] border-[#4BAE4F]'
+						className='items relative flex h-[44px] w-[44px] flex-wrap rounded-[5px] border-[2px] border-[#4BAE4F]'
 					>
-						<div className='h-[20px] w-[20px] rounded-tl-[4px] bg-[#FFE600]'></div>
-						<div className='h-[20px] w-[20px] rounded-tr-[4px] bg-[#0075FF]'></div>
+						<div className='h-[20px] w-1/2 rounded-tl-[4px] bg-[#FFE600]'></div>
+						<div className='h-[20px] w-1/2 rounded-tr-[4px] bg-[#0075FF]'></div>
 						<div className=''></div>
 						<div className='absolute -bottom-[8px] left-[35%]'>
 							<Accept width='16' height='16' />
@@ -35,8 +35,8 @@ export const ThemeSwitcher = () => {
 						onClick={() => setTheme('theme2')}
 						className='flex h-[44px] w-[44px] flex-wrap rounded-[5px] border-[2px] border-[#FAFAFA]'
 					>
-						<div className='h-[20px] w-[20px] rounded-tl-[4px] bg-[#EF1B1A]'></div>
-						<div className='h-[20px] w-[20px] rounded-tr-[4px] bg-[#3E3E3E]'></div>
+						<div className='h-[20px] w-1/2 rounded-tl-[4px] bg-[#EF1B1A]'></div>
+						<div className='h-[20px] w-1/2 rounded-tr-[4px] bg-[#3E3E3E]'></div>
 						<div className=''></div>
 					</button>
 				</li>
@@ -45,8 +45,8 @@ export const ThemeSwitcher = () => {
 						onClick={() => setTheme('theme3')}
 						className='flex h-[44px] w-[44px] flex-wrap rounded-[5px] border-[2px] border-[#FAFAFA]'
 					>
-						<div className='h-[20px] w-[20px] rounded-tl-[4px] bg-[#00C96E]'></div>
-						<div className='h-[20px] w-[20px] rounded-tr-[4px] bg-[#929293]'></div>
+						<div className='h-[20px] w-1/2 rounded-tl-[4px] bg-[#00C96E]'></div>
+						<div className='h-[20px] w-1/2 rounded-tr-[4px] bg-[#929293]'></div>
 						<div className=''></div>
 					</button>
 				</li>
@@ -55,8 +55,8 @@ export const ThemeSwitcher = () => {
 						onClick={() => setTheme('theme4')}
 						className='flex h-[44px] w-[44px] flex-wrap rounded-[5px] border-[2px] border-[#FAFAFA]'
 					>
-						<div className='h-[20px] w-[20px] rounded-tl-[4px] bg-[#0042D7]'></div>
-						<div className='h-[20px] w-[20px] rounded-tr-[4px] bg-[#DF2323]'></div>
+						<div className='h-[20px] w-1/2 rounded-tl-[4px] bg-[#0042D7]'></div>
+						<div className='h-[20px] w-1/2 rounded-tr-[4px] bg-[#DF2323]'></div>
 						<div className=''></div>
 					</button>
 				</li>
@@ -65,8 +65,8 @@ export const ThemeSwitcher = () => {
 						onClick={() => setTheme('theme5')}
 						className='flex h-[44px] w-[44px] flex-wrap rounded-[5px] border-[2px] border-[#FAFAFA]'
 					>
-						<div className='h-[20px] w-[20px] rounded-tl-[4px] bg-[#8600CB]'></div>
-						<div className='h-[20px] w-[20px] rounded-tr-[4px] bg-[#FFDE00]'></div>
+						<div className='h-[20px] w-1/2 rounded-tl-[4px] bg-[#8600CB]'></div>
+						<div className='h-[20px] w-1/2 rounded-tr-[4px] bg-[#FFDE00]'></div>
 						<div className=''></div>
 					</button>
 				</li>
